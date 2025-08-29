e9354e90e5e26ab451e2cc2c4c3305edc400d54b
Selorm Ayornu <95534257+yoselorm@users.noreply.github.com>
2025-08-29 00:21:29 +0000

    preparing tp push for live

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin',`${process.env.FRONTEND_URL}`, `http://localhost:3000`);
+  res.header('Access-Control-Allow-Origin',`${process.env.FRONTEND_URL}`);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
